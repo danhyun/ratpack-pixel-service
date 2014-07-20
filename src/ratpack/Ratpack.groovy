2f770aac62abf28e66a256eb50314f40be479a2c
Luke Daley <ld@ldaley.com>
2014-07-21 09:11:19 +1000

    Hold the data in memory instead of reading from disk every time.

--- src/ratpack/Ratpack.groovy
+++ src/ratpack/Ratpack.groovy
@@ -2,6 +2,8 @@ import com.hyunlabs.PixelModule
 import com.hyunlabs.PixelService
 import ratpack.codahale.metrics.CodaHaleMetricsModule
 
+import java.nio.file.Files
+
 import static ratpack.groovy.Groovy.ratpack
 
 ratpack {
@@ -11,16 +13,20 @@ ratpack {
     }
 
     handlers { PixelService pixelService ->
+        def pixel = Files.newInputStream(launchConfig.baseDir.file("public/blank.gif")).bytes
+
         get {
             response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate")
             response.headers.set("Pragma", "no-cache")
             response.headers.set("Expires", "0")
+            response.headers.set("Content-Type", "image/gif")
+            response.headers.set("Content-Length", pixel.length)
 
             fork {
                 pixelService.track(request)
             }
 
-            render new File('public/blank.gif').toPath()
+            response.send(pixel)
         }
     }
 
