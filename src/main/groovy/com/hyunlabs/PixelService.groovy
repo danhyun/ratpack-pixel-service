36075fd75361dde8d6763082098d4168ffc0ce33
Luke Daley <ld@ldaley.com>
2014-07-21 09:03:42 +1000

    Make logging work.

--- src/main/groovy/com/hyunlabs/PixelService.groovy
+++ src/main/groovy/com/hyunlabs/PixelService.groovy
@@ -1,14 +1,19 @@
 package com.hyunlabs
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import ratpack.http.Request
 
 class PixelService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PixelService)
+
     void track(Request request) {
         println "=== Incoming Request ==="
         def headers = request.headers
-        println new Date().format("yyyy-MM-dd HH:mm:ss")
-        println "\t${request.method.name} ${request.uri}"
-        headers.names.sort().each { println "\t${it}: ${headers.getAll(it)}"}
+        LOGGER.info new Date().format("yyyy-MM-dd HH:mm:ss")
+        LOGGER.info "\t${request.method.name} ${request.uri}"
+        headers.names.sort().each { LOGGER.info "\t${it}: ${headers.getAll(it)}"}
         println "=" * 24
     }
 }
